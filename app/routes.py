09291d6ca7d8f284c1e990d2cb4d73324987505b
DmitriyP31 <pltd31@gmail.com>
2026-01-18 11:27:02 +0500

    Task 14

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
+import re
 
 
 @app.route("/")
@@ -15,3 +16,29 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    message = request.form.get("message").strip()
+    EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+    errors = {}
+
+    if not name:
+        errors['name'] = "The name field must not be empty"
+
+    if not email:
+        errors['email'] = "The email field must not be empty"
+    elif not EMAIL_REGEX.match(email):
+        errors['email'] = "Invalid email format"
+
+    if not message:
+        errors['message'] = "The message field must not be empty"
+
+    if errors:
+        return render_template("contact.html", errors=errors, values=request.form)
+
+    return redirect(url_for("contact", status='success'))
+
